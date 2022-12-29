41fcb1e0eb30de6f300fc55d9984f22f5fe13a0a
donetica2020 <mac@Macs-MacBook-Pro.local>
2022-12-30 01:06:41 +0500

    changed port name

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
+
 
 app.get('/',(req,res) =>{
     res.send('Hello world');
     res.end()
 })
 
-app.listen(3000,()=>{
-    console.log('Wazzapp')
+app.listen(port,()=>{
+    console.log(port)
 })
 
 
